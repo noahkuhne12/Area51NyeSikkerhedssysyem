1b663aa0fa8471af17f3c1de91cf634df3834acd
noahkuhne12 <50324963+noahkuhne12@users.noreply.github.com>
2020-10-30 10:18:22 +0100

    Add project files.

---
+++ Area51NyeSikkerhedssysyem/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Area51NyeSikkerhedssysyem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
