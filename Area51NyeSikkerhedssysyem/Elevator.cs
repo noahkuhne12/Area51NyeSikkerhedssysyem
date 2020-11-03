f722ffe48545a6fa4c168f4dc379d3408df393fe
noahkuhne12 <50324963+noahkuhne12@users.noreply.github.com>
2020-11-03 09:06:21 +0100

    -Add propertys to Program and Elevator

--- Area51NyeSikkerhedssysyem/Elevator.cs
+++ Area51NyeSikkerhedssysyem/Elevator.cs
@@ -6,5 +6,17 @@ namespace Area51NyeSikkerhedssysyem
 {
     class Elevator
     {
+        public List<int> Orderlist { get; set; }
+
+        private int StartFlor 
+        {
+            get { return StartFlor; } 
+            set { StartFlor = 0; } 
+        }
+        public int Capacity 
+        {
+            get { return Capacity; } 
+            set { Capacity = 1; } 
+        }
     }
 }
--- Area51NyeSikkerhedssysyem/Program.cs
+++ Area51NyeSikkerhedssysyem/Program.cs
@@ -5,8 +5,13 @@ namespace Area51NyeSikkerhedssysyem
 {
     class Program
     {
+        public List<Flor> FlorList { get; set; }
+        public List<Staff> StaffList { get; set; }
+
         static void Main(string[] args)
         {
+            var elevator = new Elevator();
+
         }
     }
 }
