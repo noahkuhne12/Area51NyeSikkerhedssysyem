8c502be88910afc229a65f1534ac0ccdb4df8ce6
noahkuhne12 <50324963+noahkuhne12@users.noreply.github.com>
2020-11-04 10:54:48 +0100

    - add property to Staff class - add a method CreatePersonale

--- Area51NyeSikkerhedssysyem/Staff.cs
+++ Area51NyeSikkerhedssysyem/Staff.cs
@@ -6,5 +6,33 @@ namespace Area51NyeSikkerhedssysyem
 {
     public class Staff
     {
+        public int ID { get; set; }
+        public int SecurityCertificates { get; set; }
+        public int SpawnFlor { get; set; }
+        public int TagetFlor { get; set; }
+        public int SpawnTime { get; set; }
+        public bool DieState { get; set; }
+
+        public static void CreatePersonale(int numberForPersonale)
+        {
+            Database database = new Database();
+
+            for (int i = 0; i <= numberForPersonale; i++)
+            {
+                database.StaffList.Add(new Staff());
+                database.StaffList[i].ID = i;
+                database.StaffList[i].SecurityCertificates = Random(0, 5);
+                database.StaffList[i].SpawnFlor = Random(0,3);
+                database.StaffList[i].TagetFlor = Random(0,3);
+                database.StaffList[i].SpawnTime = Random(0,100);
+                database.StaffList[i].DieState = true;
+            }
+        }
+        private static int Random(int num1, int num2)
+        {
+            int random_number = new Random().Next(num1, num2);
+
+            return random_number;
+        }
     }
 }
